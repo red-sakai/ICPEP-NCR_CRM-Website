e2363d8167c0a9c78bcc89860d6c80bfb211e929
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-10-29 17:03:41 +0800

    feat (Closes #4): add about vision section mobile view

--- src/sections/Vision.tsx
+++ src/sections/Vision.tsx
@@ -1,21 +1,72 @@
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+type BuildCardProps = {
+  className?: string;
+};
+const BuildCard = ({ className = "" }: BuildCardProps) => {
+  return (
+    <Card
+      className={cn(
+        "flex-1 py-30 px-8 flex flex-col justify-center items-center",
+        "bg-primary/10 border-2 border-primary/15 gap-2",
+        "hover:[&>h1]:scale-110 hover:[&>span]:scale-110",
+        "transition-all duration-300",
+        className,
+      )}
+    >
+      <h1 className="text-8xl text-glow tracking-tighter font-medium text-primary/15 transition-all duration-300">
+        {"{ }"}
+      </h1>
+      <span className="text-xs sm:text-sm text-foreground/60 text-center transition-all duration-300">
+        Building the future of tech
+      </span>
+    </Card>
+  );
+};
 
 const Vision = () => {
   return (
     <section className="w-full flex justify-center items-center">
       <div className="container">
-        <div className="w-full flex justify-center items-center gap-12 p-40">
+        <div className="w-full h-screen flex justify-center items-center gap-12 p-10 md:p-20 lg:40">
           <div className="flex-1 space-y-8">
-            <h1 className="text-3xl font-sans text-glow text-primary font-bold tracking-tight">
-              Our Vision
-            </h1>
-            <p className="text-foreground/60 leading-7 font-sans">
-              To be the leading platform for computer science professionals in
-              the NCR region, fostoring innovation, collaboration, and
-              excellence in technology.
-            </p>
-            <ul className="list-disc pl-2 font-sans space-y-3">
+            {/* desktop view */}
+            <div className="hidden md:block space-y-8">
+              <h1 className="text-2xl md:text-3xl font-sans text-glow text-primary font-bold tracking-tight">
+                Our Vision
+              </h1>
+              <p className="md:text-lg text-foreground/60 leading-6 md:leading-7 font-sans">
+                To be the leading platform for computer science professionals in
+                the NCR region, fostoring innovation, collaboration, and
+                excellence in technology.
+              </p>
+            </div>
+
+            {/* mobile view */}
+            <Accordion type="single" collapsible className="py-0 mb-0">
+              <AccordionItem value="vission" className="md:hidden space-y-2">
+                <AccordionTrigger className="text-2xl md:text-3xl font-sans text-glow text-primary font-bold tracking-tight">
+                  Our Vision
+                </AccordionTrigger>
+                <AccordionContent className="md:text-lg text-foreground/60 leading-6 md:leading-7 font-sans">
+                  To be the leading platform for computer science professionals
+                  in the NCR region, fostoring innovation, collaboration, and
+                  excellence in technology.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+
+            {/* mobile view */}
+            <BuildCard className="lg:hidden py-20 [&>h1]:text-6xl [&>span]:text-xs" />
+
+            <ul className="list-disc text-sm md:text-lg pl-4 font-sans space-y-3">
               <li className="marker:text-primary">Empower professionals</li>
               <li className="marker:text-primary">Drive Innovation</li>
               <li className="marker:text-primary">Build Community</li>
@@ -23,21 +74,8 @@ const Vision = () => {
             </ul>
           </div>
 
-          <Card
-            className={cn(
-              "flex-1 py-30 flex flex-col justify-center items-center",
-              "bg-primary/10 border-2 border-primary/15 gap-2",
-              "hover:[&>h1]:scale-110 hover:[&>span]:scale-110",
-              "transition-all duration-300",
-            )}
-          >
-            <h1 className="text-8xl text-glow tracking-tighter font-medium text-primary/15 transition-all duration-300">
-              {"{ }"}
-            </h1>
-            <span className="text-sm text-foreground/60 transition-all duration-300">
-              Building the future of tech
-            </span>
-          </Card>
+          {/* desktop view */}
+          <BuildCard className="hidden lg:flex" />
         </div>
       </div>
     </section>
