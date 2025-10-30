745c28f61bea539950ab08d97988640fb554ab58
CARL BLANCAFLOR <carlfblancaflor@iskolarngbayan.pup.edu.ph>
2025-10-30 13:20:45 +0800

    fix: merge issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import NavBar from "./sections/NavBar";
-import JoinUsPage from "./pages/JoinUsPage";
-
 function App() {
   useEffect(() => {
     document.documentElement.classList.add("dark");
@@ -19,11 +17,6 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/join-us" element={<JoinUsPage />} />
-      </Routes>
     </BrowserRouter>
   );
 }
