90b5bda7bb2de4b74286ea036d37fa76da0c593a
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-10-30 12:15:14 +0800

    style (Closes #6): adjust join us page decoration

--- src/sections/JoinUs.tsx
+++ src/sections/JoinUs.tsx
@@ -8,7 +8,7 @@ const JoinUs = () => {
           <div className="absolute -z-10 top-60 md:top-40 right-10 lg:right-20 blur-circle size-30 lg:size-40 rounded-full bg-primary/15 blur-xl" />
           <div className="absolute -z-10  bottom-20 md:-bottom-10 md:left-30 lg:left-50 blur-circle size-50 lg:size-60 rounded-full bg-primary/15 blur-2xl" />
           <div className="hidden md:block absolute -z-10 bottom-30 md:bottom-20 blur-circle size-20 lg:size-30 rounded-full bg-primary/15 blur-xl" />
-          <div className="hidden sm:block absolute -z-10 -bottom-10 right-0 lg:right-20 blur-circle size-40 md:size-60 lg:size-70 rounded-full bg-primary/15 blur-2xl" />
+          <div className="hidden sm:block absolute -z-10 -bottom-10 right-0 lg:right-10 blur-circle size-40 md:size-60 lg:size-70 rounded-full bg-primary/15 blur-2xl" />
           <h1 className="text-4xl md:text-6xl text-primary text-center text-glow font-sans font-bold">
             ICpEP.se NCR
           </h1>
